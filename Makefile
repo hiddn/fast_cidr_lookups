81a598152b019c7ab943055f52f6e822c303fde9
Hidden <hidden@undernet.org>
2025-01-22 21:20:19 -0500

    Add example.c

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ OBJ_DIR := obj
 BIN_DIR := .
 TARGET := $(BIN_DIR)/cidr
 
+EXAMPLE_SRC := example.c
+EXAMPLE_OBJ := $(OBJ_DIR)/example.o
+
 # Source and Object files
 SRCS := $(wildcard $(SRC_DIR)/*.c)
 OBJS := $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
@@ -14,13 +17,21 @@ OBJS := $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
 # Targets
 .PHONY: all test clean
 
-all: $(TARGET)
+all: $(TARGET) $(BIN_DIR)/example
 
 $(TARGET): $(OBJS) $(TEST_DIR)/tests.c
 	@mkdir -p $(BIN_DIR)
 	$(CC) $(CFLAGS) -o $@ $(OBJS) $(TEST_DIR)/tests.c
 
+$(BIN_DIR)/example: $(EXAMPLE_OBJ) $(OBJS)
+	@mkdir -p $(BIN_DIR)
+	$(CC) $(CFLAGS) -o $@ $^
+
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(OBJ_DIR)/example.o: example.c
 	@mkdir -p $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c -o $@ $<
 
---
+++ example.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/cidr_lookups.h"
+#include "include/irc_stuff.h"
+
+int main() {
+    char *str1 = "Data 1";
+    char *str2 = "Data 2";
+    char *str3 = "Data 3";
+    char *str4 = "Data 4";
+    char *str5 = "New data";
+    char *str_ptr;
+    cidr_node *node;
+    cidr_root_node *root_tree = cidr_new_tree();
+
+    cidr_add_node(root_tree, "1234:5678:90ab:cdef::1/128", str1);
+    cidr_add_node(root_tree, "1.2.3.0/24", str2);
+    cidr_add_node(root_tree, "1.2.0.0/20", str3);
+    cidr_add_node(root_tree, "1.2.3.4/32", str4);
+    cidr_add_node(root_tree, "1.2.3.4/32", str5);  // This changes the data of the node (str4 -> str5)
+
+    // Get the data directly
+    str_ptr = (char *)cidr_get_data(root_tree, "1234:5678:90ab:cdef::1/128");
+    if (str_ptr) {
+        printf("Data for node 1234:5678:90ab:cdef::1/128: %s\n", str_ptr);
+    }
+
+    // Get node infos
+    node = cidr_find_node(root_tree, "1.2.3.0/24");
+    if (node) {
+        str_ptr = (char *)node->data;
+        char cidr[CIDR_LEN+1];
+        set_cidr_mask(node, cidr);
+        printf("Data for node %s: %s\n", cidr, str_ptr);
+        node->data = str5;
+        printf("Data for node %s: %s\n", cidr, str_ptr);
+        char parent_cidr[CIDR_LEN+1];
+        set_cidr_mask(node->parent, parent_cidr);
+        printf("Node %s has parent %s\n", cidr, parent_cidr);
+    }
+
+    // Remove a node
+    int is_rem_success = cidr_rem_node_by_cidr(root_tree, "1.2.0.0/20");
+    if (is_rem_success)
+        printf("Sucessfully removed node 1.2.0.0/20\n");
+    node = cidr_find_node(root_tree, "1.2.0.0/20");
+    if (!node) {
+        printf("Node 1.2.0.0/20 not found. It was indeed removed.\n");
+    }
+
+    return 0;
+}
