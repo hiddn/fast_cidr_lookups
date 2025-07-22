4b2f6be9382bf49394ae64f507535f1e839d610f
Hidden <hidden@undernet.org>
2025-07-22 16:26:08 -0400

    Adding header comments to almost all .c and .h files

--- src/cidr_lookups.c
+++ src/cidr_lookups.c
@@ -1,3 +1,23 @@
+/*
+ * fast_cidr_lookups - cidr_lookups.c
+ * Copyright (C) 2025 Hidden <hidden@undernet.org> and
+ *                    Entrope <entrope@undernet.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <limits.h> /* for CHAR_BIT */
 #include <stdio.h> /* for printf, snprintf */
 #include <stdlib.h> /* for malloc, free, exit */
